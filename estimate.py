22049c5cf11bdb97067c3e20f02f95310aa6dc0b
Arujnmk06 <arjunmk2506@gmail.com>
2021-06-01 20:11:11 +0530

    adds two functions

--- estimate.py
+++ estimate.py
@@ -1,5 +1,12 @@
 import math
 import unittest
+import random
+def wallis(i):
+    sum=1
+    for n in range(1,i+1):
+         sum=sum*(4*n*n)/(4*n*n-1)
+         
+    return sum*2
 
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
@@ -12,6 +19,23 @@ class TestWallis(unittest.TestCase):
             pi = wallis(i)
             self.assertTrue(abs(pi - math.pi) < 0.01, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
 
+
+def monte_carlo(n):
+       c=0
+       d=0
+       
+       for n in range(1,n+1):
+              x=random.random()
+              y=random.random()
+              
+              d=((x*x)+(y*y))**0.5
+              
+              if(d<=1):
+                   c=c+1
+              
+       p=c/n
+       pi=4*p
+       return pi
 
 class TestMC(unittest.TestCase):
     def test_randomness(self):
